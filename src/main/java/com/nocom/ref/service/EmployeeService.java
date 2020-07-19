3391d8516415fa16c45b16b57d02c646a09ee31e
admin <rong@agile-solutions.ch>
2020-07-19 14:11:03 +0200

    security

--- src/main/java/com/nocom/ref/service/EmployeeService.java
+++ src/main/java/com/nocom/ref/service/EmployeeService.java
@@ -1,4 +1,25 @@
 package com.nocom.ref.service;
 
+import com.nocom.ref.model.Employee;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@Service
 public class EmployeeService {
+
+    private EmployeeService employeeService;
+
+    @PreAuthorize("@securityUtils.isManager()")
+    public List<Employee> findAdultMale() {
+
+
+
+        return employeeService.findAdultMale();
+
+    }
 }
--- src/main/java/com/nocom/ref/util/SecurityUtils.java
+++ src/main/java/com/nocom/ref/util/SecurityUtils.java
@@ -2,9 +2,11 @@ package com.nocom.ref.util;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.function.Predicate;
 
+@Component("securityUtils")
 public class SecurityUtils {
 
     private static final String ROLE_MANAGER = "ROLE_MANAGER";
